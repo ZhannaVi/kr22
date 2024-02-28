9296e50f63b874e105fdc95360f77e73bdd70e98
ZhannaVi <129753574+ZhannaVi@users.noreply.github.com>
2024-02-28 12:26:01 +0300

    Add files via upload

---
+++ kr22/kr22/Products.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;       // EXCEL APPLICATION.
+using System.Drawing;
+
+namespace kr22
+{
+    public partial class Products : Form
+    {
+        public Products()
+        {
+            InitializeComponent();
+            this.dataGridView1.RowStateChanged += new System.Windows.Forms.DataGridViewRowStateChangedEventHandler(dataGridView1_RowStateChanged);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
